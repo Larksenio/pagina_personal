65d315719b5a87e0cba86e0b2b848cc02315da2a
larksenio <jamiller@utpl.edu.ec>
2024-10-17 00:45:26 -0500

    actual

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
-import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PortafolioComponent } from './portafolio/portafolio.component'; // Cambiamos a PortafolioComponent
-
+import { Component, HostListener } from '@angular/core';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -11,4 +10,14 @@ import { PortafolioComponent } from './portafolio/portafolio.component'; // Camb
 })
 export class AppComponent {
   title = 'pagina_personal';
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    const header = document.querySelector('header');
+    if (window.pageYOffset > 50) { // Cambia el valor según cuándo quieras cambiar el color
+      header?.classList.add('scrolled');
+    } else {
+      header?.classList.remove('scrolled');
+    }
+  }
 }
